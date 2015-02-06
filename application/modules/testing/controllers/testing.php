e7157d9baad89225f2d7a2b0381d9ca2b9fbd60e
Yanuar Risqi Assrullah <assrullah@gmail.com>
2015-02-06 11:27:16 +0700

    Menambah fontello

--- application/modules/testing/controllers/testing.php
+++ application/modules/testing/controllers/testing.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Testing extends CI_Controller {
+class Testing extends MX_Controller {
 
 	public function __construct()
 	{
